3807411bb7a24d57b48ea67e635ef73ffd44d622
Sebastian Theophil <stheophil@think-cell.com>
2022-12-08 17:21:11 +0100

    Update includes

--- range.example.cpp
+++ range.example.cpp
@@ -6,9 +6,10 @@
 // Distributed under the Boost Software License, Version 1.0.
 // See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
 
-#include "tc/range.h"
-#include "tc/format.h"
-#include "tc/make_c_str.h"
+#include "tc/range/meta.h"
+#include "tc/range/filter_adaptor.h"
+#include "tc/string/format.h"
+#include "tc/string/make_c_str.h"
 
 #include <boost/range/adaptors.hpp>
 
